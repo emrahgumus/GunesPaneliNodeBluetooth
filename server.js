a8e47f6fc9f5c9396be360f60550e0a43fe8cb2d
Emrah Gümüş <emrahgumus34@gmail.com>
2017-01-24 18:19:16 +0300

    jmeter test dosyası eklendi

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-var socket = require('socket.io-client')('http://localhost:4000');
+var socket = require('socket.io-client')('https://yapbenzet.com:4000');
 
-var d_akim = 50;
-var d_geri = 40;
-var d_sica = 30;
-var d_nem  = 20;
-var d_ligh = 10;
+var d_akim = 17;
+var d_geri = 17;
+var d_sica = 17;
+var d_nem  = 17;
+var d_ligh = 17;
 
 function rand(sayi){
 
@@ -29,7 +29,7 @@ socket.on('connect', function(){
   timer = setInterval(function(){
 
     var veriler = {
-      panelId: '583fe76c9496e36605f923b9',
+      panelId: '588766cb9398c243182ff337',
       akim: d_akim = rand(d_akim),
       gerilim: d_geri = rand(d_geri),
       light: d_ligh = rand(d_ligh),
@@ -38,7 +38,7 @@ socket.on('connect', function(){
      };
     socket.compress(false).emit('verileriKaydetDagit', veriler);
 
-    var veriler = {panelId: '583fe7759496e36605f923ba', akim: d_akim = rand(d_akim), gerilim: d_geri = rand(d_geri), light: d_ligh = rand(d_ligh), sicaklik: d_sica = rand(d_sica), nem: d_nem = rand(d_nem)};
+    var veriler = {panelId: '584916dfcae05eaf031cda5e', akim: d_akim = rand(d_akim), gerilim: d_geri = rand(d_geri), light: d_ligh = rand(d_ligh), sicaklik: d_sica = rand(d_sica), nem: d_nem = rand(d_nem)};
     socket.compress(false).emit('verileriKaydetDagit', veriler);
     console.log("gonderdi");
 
